816f94e671a452b9f9ff364d3c883d9c4603e429
Milene <milenefaria33@gmail.com>
2024-10-22 08:29:21 -0300

    fix: finding instructor by email and role

--- src/main/java/br/com/alura/ProjetoAlura/user/UserService.java
+++ src/main/java/br/com/alura/ProjetoAlura/user/UserService.java
@@ -2,13 +2,9 @@ package br.com.alura.ProjetoAlura.user;
 
 import br.com.alura.ProjetoAlura.util.DomainException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -17,20 +13,9 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public User getInstructorByEmail(String email) {
-        Optional<User> userOpt = repository.findByEmail(email);
-
-        if (!userOpt.isPresent()) {
-            log.error("Error finding instructor by email address: no registered user.");
-            throw new DomainException("There is no registered user with the email address informed, please try again.");
-        }
-
-        User user = userOpt.get();
-        if (!Role.INSTRUCTOR.equals(user.getRole())) {
-            log.error("Error finding instructor by email adress: email address does not belong to an instructor.");
-            throw new DomainException("The email address provided is not from an instructor, please try again.");
-        }
-
-        return user;
+        return repository.findByEmailAndRole(email, Role.INSTRUCTOR).orElseThrow(
+                () -> new DomainException("There is no registered instructor with the email address informed, please try again.")
+        );
     }
 
     @Transactional(readOnly = true)
